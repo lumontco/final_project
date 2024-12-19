b09085030b1a3bf6cce2f16d4da376fc8587aefe
carllumont <lugomontero@gmail.com>
2024-12-19 12:20:09 -0500

    Task 6.

---
+++ server.py
@@ -0,0 +1,30 @@
+# Import Flask, render_template, request from the flask pramework package
+from flask import Flask, render_template, request
+# Import the sentiment_analyzer function from the package created
+from EmotionDetection.emotion_detection import emotion_detector
+
+
+#Initiate the flask app
+app = Flask("Emotion Detector")
+
+@app.route("/emotionDetector")
+def emot_detector():
+    # Retrieve the text to analyze from the request arguments
+    text_to_analyze = request.args.get('textToAnalyze')
+    # Pass the text to the sentiment_analyzer function and store the response
+    response = emotion_detector(text_to_analyze)
+    # Extract the label and score from the response
+    customer_response = response["emotionPredictions"][0]["emotion"]
+    # Return a formatted string with the sentiment label and score
+    return ("For the given statement, the system response is {}.".format(customer_response)
+            .replace('{','').replace('}',''))
+
+@app.route("/")
+def render_index_page():
+    ''' This function initiates the rendering of the main application
+        page over the Flask channel
+    '''
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=9000)
